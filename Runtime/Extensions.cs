8d8eab4f947304f63e0f6fef854632f8729628fb
Pham Duc <ducpt@valofe.com>
2024-07-09 16:15:10 +0700

    + Add Extensions

--- Runtime/Extensions.cs
+++ Runtime/Extensions.cs
@@ -1,3 +1,6 @@
+using FMOD;
+using FMOD.Studio;
+
 public static class Extensions 
 {
     public static void Release(this FMOD.Studio.EventInstance eventInstance)
@@ -7,5 +10,20 @@ public static class Extensions
             eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             eventInstance.release();
         }
+    }
+
+    public static void SetParameterByName(this EventInstance eventInstance, string parameterName, float value)
+    {
+        eventInstance.setParameterByName(parameterName, value);
+    }
+
+    public static RESULT GetParameterByName(this EventInstance eventInstance, string parameterName, out float value)
+    {
+        return eventInstance.getParameterByName(parameterName, out value);
+    }
+
+    public static RESULT SetVolume(this Bus bus, float volume)
+    {
+        return bus.setVolume(volume);
     }
 }
